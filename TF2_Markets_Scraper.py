b447f476b4d13a04d2afa2a0c4e6dcfdc7ef1bbf
Sweaver273 <71162829+Sweaver273@users.noreply.github.com>
2023-05-14 18:20:21 -0500

    Re-Add Support for Skinned Items Across All Sites
    Code is a bit messier now, but worth the results.
    Also added support for searching by effect on Steam Community Market.
    Minor bug fixes.

--- TF2_Markets_Scraper.py
+++ TF2_Markets_Scraper.py
@@ -10,6 +10,10 @@ while True:
         #Steam Listings
         global steamcommunity
         global isUncraftable
+        oldname = market_hash_name
+        market_hash_name = re.sub(r'^(?:(?P<effect>Community Sparkle|Cool|Energy Orb|Hot|Isotope)\s)(?:(?P<quality>Strange|Vintage|Collectors|Strange Geniune|Strange Haunted|Genuine|Haunted)\s)?(Unusual)\s?(?:(?P<festivized>Festivized)\s)?(?:(?P<killstreak>Specialized Killstreak|Professional Killstreak|Killstreak)\s)?(?P<item>.*)$', r'\2 \3 \4 \5?filter=\1', market_hash_name)
+        market_hash_name = re.sub(r'^(.*)\s(?:(Strange)\s)(Unusual)\s(.*)$', r'\2 \3 \4?filter=\1', market_hash_name) #for hats/cosmetics
+        market_hash_name = re.sub(r'^(.*)\s(Unusual)\s(.*)$', r'\2 \3?filter=\1', market_hash_name) #for hats/cosmetics
         response = requests.get(f"https://steamcommunity.com/market/listings/440/{market_hash_name}")
         print(f"https://steamcommunity.com/market/listings/440/{market_hash_name}")
         if response.status_code == 200:
@@ -17,26 +21,38 @@ while True:
             marketid = re.search(r'Market_LoadOrderSpread.*?(\d+)', response.text)
             if marketid is None:
                 steamcommunity = ['Steam Community', 'Not on Market', 'Not on Market', 'Not on Market', 'Not on Market']
-                isUncraftable = False
+                isUncraftable = False #This is a bit of an assumption, may need to figure out something better in the future
                 return
             isUncraftable = bool(re.search(r'Not Usable in Crafting', response.text))
+            if market_hash_name != oldname:
+                lowestsellr = re.search(r'market_listing_price market_listing_price_with_fee">\s*.*?(\S*)\s(.*\d)', response.text) #this is set to capture the currency type since we cannot use the backend to translate it for us given the use of special params.
+                if lowestsellr is None:
+                    lowestsell = "No Listings"
+                    LSactualreturn = "No Listings"
+                else:
+                    lowestsell = lowestsellr.group(2).replace(".", '')
+                    lowestsell = lowestsell.replace(",", '.')
+                    LSactualreturn = lowestsellr.group(1) + str(round((float(lowestsell) / 1.15) + .005, 2))  #ceil() for 2 decimal places
+                    lowestsell = lowestsellr.group(1) + lowestsell
         else:
             raise(f"Steam returned code {response.status_code} fetching sell orders. This shouldn't happen.")
-        response = requests.get(f"https://steamcommunity.com/market/priceoverview/?appid=440&currency=1&market_hash_name={market_hash_name}")
-        if response.status_code == 200:
-            lowestsellr = re.search(r'lowest_price":"\$(\S{1,6}\.\d{2})",', response.text)
-            if lowestsellr is None:
-                lowestsell = "No Listings"
-                LSactualreturn = "No Listings"
+
+        if market_hash_name == oldname:
+            response = requests.get(f"https://steamcommunity.com/market/priceoverview/?appid=440&currency=1&market_hash_name={market_hash_name}") #This link converts the prices into USD, although it doesn't accept parameters :/
+            if response.status_code == 200:
+                lowestsellr = re.search(r'lowest_price":"\$(\S{1,6}\.\d{2})"', response.text)
+                if lowestsellr is None:
+                    lowestsell = "No Listings"
+                    LSactualreturn = "No Listings"
+                else:
+                    lowestsell = "$" + lowestsellr.group(1)
+                    LSactualreturn = "$" + str(round((float(lowestsellr.group(1)) / 1.15) + .005, 2))  #ceil() for 2 decimal places
+            elif response.status_code == 429:
+                lowestsell = "Ratelimited"
+                LSactualreturn = "Ratelimited"
             else:
-                lowestsell = "$" + lowestsellr.group(1)
-                LSactualreturn = "$" + str(round((float(lowestsellr.group(1)) / 1.15) + .005, 2))  #ceil() but mathimatically
-        elif response.status_code == 429:
-            lowestsell = "Ratelimited"
-            LSactualreturn = "Ratelimited"
-        else:
-            lowestsell = "N/A"
-            BOactualreturn = "N/A"
+                lowestsell = "N/A"
+                LSactualreturn = "N/A"
 
         #Steam Buyorders
         response = requests.get(f"https://steamcommunity.com/market/itemordershistogram?country=US&language=english&currency=1&item_nameid={marketid.group(1)}&two_factor=0")
@@ -48,7 +64,7 @@ while True:
             else:
                 BOactualreturn = highestbuyr.group(1).replace(",", "")
                 highestbuy = "$" + BOactualreturn
-                BOactualreturn = "$" + str(round((float(BOactualreturn) / 1.15) + .005, 2))  #ceil() but mathimatically
+                BOactualreturn = "$" + str(round((float(BOactualreturn) / 1.15) + .005, 2))  #ceil() for 2 decimal places
         elif response.status_code == 429:
             highestbuy = "Ratelimited"
             BOactualreturn = "Ratelimited"
@@ -61,8 +77,10 @@ while True:
 
     def MCPrices(name, driver):
         name = name.replace(" ", "-")
-        name = name.replace(".", "")
-        name = name.replace(":", "")
+        punc = '''!()[]{};:'"\,<>./?@#$%^&*_~'''
+        for ele in name:
+            if ele in punc:
+                name = name.replace(ele, "")
         name = name.lower()
         if isUncraftable == True:
             url = f"https://mannco.store/item/440-uncraftable-{name}"
@@ -70,28 +88,33 @@ while True:
             url = f"https://mannco.store/item/440-{name}"
         driver.get(url)
         print(f"{url}")
-        lowestsellr = re.search(r'lowPrice": "(\d+\.\d{2})', driver.page_source) #r'ecurrency">\$(\d+\.\d{2})'
+        lowestsellr = re.search(r'lowPrice": "(\d+\.\d{2})', driver.page_source)
         highestbuyr = re.search(r'Quantity(?:<.*>\s)*?<td>\$(\S{1,6}\.\d{2})', driver.page_source)
-        if lowestsellr.group(1) == "0.00":
+        if lowestsellr is None or lowestsellr.group(1) == "0.00":
             lowestsell = "No Listings"
             LSactualreturn = "No Listings"
         else:
             lowestsell = "$" + lowestsellr.group(1)
-            LSactualreturn = "$" + str(round((float(lowestsellr.group(1)) / 1.05) + .005, 2))  #ceil() but mathimatically
+            LSactualreturn = "$" + str(round((float(lowestsellr.group(1)) / 1.05) + .005, 2))  #ceil() for 2 decimal places
         if highestbuyr is None:
             highestbuy = "No Orders"
             BOactualreturn = "No Orders"
         else:
             BOactualreturn = highestbuyr.group(1).replace(",", "")
             highestbuy = "$" + BOactualreturn
-            BOactualreturn = "$" + str(round((float(BOactualreturn) / 1.05) + .005, 2))  #ceil() but mathimatically
+            BOactualreturn = "$" + str(round((float(BOactualreturn) / 1.05) + .005, 2))  #ceil() for 2 decimal places
         global Mannco_store
         Mannco_store = ['Mannco.store', lowestsell, LSactualreturn, highestbuy, BOactualreturn]
         return
 
     def Mkt_tfPrices(name, driver):
-        name = re.sub(r'^(Strange\s|Vintage\s|Collectors\s|Genuine\s|Haunted\s)?(Professional |Specialized )(Killstreak )(.*)$', r'\2\1\4', name)
-        name = re.sub(r'^(.*)(Unusual\s)(.*)$', r'\1\3', name)
+        name = name.replace("Killstreak", "Basic Killstreak")
+        name = name.replace("Specialized Basic Killstreak", "Specialized")
+        name = name.replace("Professional Basic Killstreak", "Professional")
+        name = re.sub(r'^(?:(?P<effect>Community Sparkle|Cool|Energy Orb|Hot|Isotope)\s)(?:(?P<quality>Strange|Vintage|Collectors|Strange Geniune|Strange Haunted|Genuine|Haunted)\s)?(?:Unusual )?(?:(?P<festivized>Festivized) )?(?:(?P<killstreak>Specialized|Professional|Basic Killstreak)\s)?(?P<item>.*)$', r'\4 \2 %E2%98%85\1 \3 \5', name)
+        name = re.sub(r'^(.*)\s(?:(Strange)\s)(?:Unusual)\s(.*)$', r'\1 \2 \3', name) #for hats/cosmetics
+        name = re.sub(r'^(.*)\s(?:Unusual)\s(.*)$', r'\1 \2', name) #for hats/cosmetics
+        name = name.replace("  ", " ")
         if isUncraftable == True:
             if name.find(" Kit") > -1:
                 url = f"https://marketplace.tf/items/tf2/{name}"
@@ -109,14 +132,14 @@ while True:
         else:
             LSactualreturn = lowestsellr.group(1).replace(",", "")
             lowestsell = "$" + LSactualreturn
-            LSactualreturn = "$" + str(round((float(LSactualreturn) / 1.10) + .005, 2))  #ceil() but mathimatically
+            LSactualreturn = "$" + str(round((float(LSactualreturn) / 1.10) + .005, 2))  #ceil() for 2 decimal places
         if highestbuyr is None:
             highestbuy = "No Orders"
             BOactualreturn = "No Orders"
         else:
             BOactualreturn = highestbuyr.group(1).replace(",", "")
             highestbuy = "$" + BOactualreturn
-            BOactualreturn = "$" + str(round((float(BOactualreturn) / 1.10) + .005, 2))  #ceil() but mathimatically
+            BOactualreturn = "$" + str(round((float(BOactualreturn) / 1.10) + .005, 2))  #ceil() for 2 decimal places
 
         global Marketplace_tf
         Marketplace_tf = ['Marketplace.tf', lowestsell, LSactualreturn, highestbuy, BOactualreturn]
