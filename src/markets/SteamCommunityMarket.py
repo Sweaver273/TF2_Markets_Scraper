b35a84a328d3bc569b2a26f35cd9c2344e329e6f
sweaver273 <sweaverisgood@gmail.com>
2025-07-30 19:51:14 -0500

    Optimize Steam profile.

--- src/markets/SteamCommunityMarket.py
+++ src/markets/SteamCommunityMarket.py
@@ -33,61 +33,64 @@ def GetPrices(item_attributes):
     s = time.time()
     r = requests.get(market_hash_url)
     print("[INFO] Steam Frontend: " + str(round((time.time()-s)*1000,0)) + " ms.")
-    assert r.status_code == 200
-    marketid = re.search(r'Market_LoadOrderSpread.*?(\d+)', r.text)
-    if marketid is None:
-        steamcommunity = ['Steam Community', 'No Marketid!', 'No Marketid!', 'No Marketid!', 'No Marketid!']
-        return steamcommunity
-    
-    #If we are searching for Unusual effects/craftability, we need to scrape from the Community Market's frontend. 
-    #Eventually, we should try to only use this request and process currency conversions to avoid API IP ratelimits.
-    #This also has a side-effect of showing each listing in the listers' native currency, which is a bit annoying.
-    if market_hash_url.find("?filter=") != -1:
-        lowestsellr = re.search(r'market_listing_price market_listing_price_with_fee">[\s.]{6,}\s(?P<curr>\D*)?(?P<value>[\d\.\,\ ]*)(?P<curr2>\D*)?\s{5}<\/', r.text)
-        if lowestsellr is None:
-            lowestsell = None
-            LSactualreturn = None
-        else:
-            lowestsell = float(re.sub(r'[^\d]', "", lowestsellr.group('value')))
-            lowestsell = lowestsell / 100
-            
-            #TODO: Review this callculation for actual return
-            LSactualreturn = lowestsellr.group('curr') + str("{:.2f}".format(round((float(lowestsell) / 1.15) + .005, 2))) + lowestsellr.group('curr2')
-            lowestsell = lowestsellr.group('curr') + "{0:.2f}".format(lowestsell) + lowestsellr.group('curr2')
-    
-    #Backend request
-        #The ratelimit is 20/min, 1000/day, which is lenient but would suck if trying to price entire inventories.
-    if market_hash_url.find("?filter=") == -1:
-        s = time.time()
-        r = requests.get(f"https://steamcommunity.com/market/priceoverview/?appid=440&currency=1&market_hash_name={market_hash_name}")
-        print("[INFO] Steam Backend: " + str(round((time.time()-s)*1000,0)) + " ms.")
-        assert r.status_code == 200 or r.status_code == 429
-        if r.status_code == 429:
-            lowestsell = "Ratelimited"
-            LSactualreturn = "Ratelimited"
-        else:
-            lowestsellr = re.search(r'lowest_price":"\$(\S{1,6}\.\d{2})"', r.text)
+    if r.status_code == 200:
+        marketid = re.search(r'Market_LoadOrderSpread.*?(\d+)', r.text)
+        if marketid is None:
+            steamcommunity = ['Steam Community', 'No Marketid!', 'No Marketid!', 'No Marketid!', 'No Marketid!']
+            return steamcommunity
+        
+        #If we are searching for Unusual effects/craftability, we need to scrape from the Community Market's frontend. 
+        if True:
+            #Regex captures currency prefixes, monetary value, then currency suffixes. May need to parse these later on for currency conversions.
+            #Currently, it seems like Steam is converting all prices to USD. Less work for me I guess. Keep this here since non-US IPs will specify USD suffix.
+            lowestsellr = re.search(r'market_listing_price market_listing_price_with_fee">[\s.]{6,}\s(?P<curr>\D*)?(?P<value>[\d\.\,\ ]*)(?P<curr2>\D*)?\s{5}<\/', r.text)
             if lowestsellr is None:
                 lowestsell = None
                 LSactualreturn = None
             else:
-                #Since we specify currency=1, USD will always be returned.
-                lowestsell = "$" + "{:.2f}".format(float(lowestsellr.group(1)))
-
-                #TODO: Review this callculation for actual return
-                LSactualreturn = "$" + str("{:.2f}".format(round((float(lowestsellr.group(1)) / 1.15) + .005, 2)))
-    
+                lowestsell = float(re.sub(r'[^\d]', "", lowestsellr.group('value')))
+                lowestsell = lowestsell / 100
+                if lowestsellr.group('curr2') == "USD":
+                    #TODO: Review this calculation for actual return
+                    LSactualreturn = lowestsellr.group('curr') + str("{:.2f}".format(round((float(lowestsell) / 1.15) + .005, 2)))
+                    lowestsell = lowestsellr.group('curr') + "{0:.2f}".format(lowestsell)
+                else:
+                    #TODO: Review this calculation for actual return
+                    LSactualreturn = lowestsellr.group('curr') + str("{:.2f}".format(round((float(lowestsell) / 1.15) + .005, 2))) + lowestsellr.group('curr2')
+                    lowestsell = lowestsellr.group('curr') + "{0:.2f}".format(lowestsell) + lowestsellr.group('curr2')
+    else:
+        print(f"Frontend returned {r.status_code}, failed to scrape ask prices.")
+        lowestsell = "Failed"
+        LSactualreturn = "Failed"
+    #Backend request
+        #The ratelimit is 20/min, 1000/day, which is lenient but would suck if trying to price entire inventories.
+    #if False: #DISABLED
+    #    s = time.time()
+    #    r = requests.get(f"https://steamcommunity.com/market/priceoverview/?appid=440&currency=1&market_hash_name={market_hash_name}")
+    #    print("[INFO] Steam Backend: " + str(round((time.time()-s)*1000,0)) + " ms.")
+    #    assert r.status_code == 200 or r.status_code == 429
+    #    if r.status_code == 429:
+    #        lowestsell = "Ratelimited"
+    #        LSactualreturn = "Ratelimited"
+    #    else:
+    #        lowestsellr = re.search(r'lowest_price":"\$(\S{1,6}\.\d{2})"', r.text)
+    #        if lowestsellr is None:
+    #            lowestsell = None
+    #            LSactualreturn = None
+    #        else:
+    #            #Since we specify currency=1, USD will always be returned.
+    #            lowestsell = "$" + "{:.2f}".format(float(lowestsellr.group(1)))
+    #
+    #            #TODO: Review this calculation for actual return
+    #            LSactualreturn = "$" + str("{:.2f}".format(round((float(lowestsellr.group(1)) / 1.15) + .005, 2)))
+    # 
     #Steam Buyorders
         #Ratelimit: 10 different items per hour. The same item is allowed to repeat w/o limit.
         #This backend API is the ONLY OPTION for grabbing buy order data.
     s = time.time()
     r = requests.get(f"https://steamcommunity.com/market/itemordershistogram?country=US&language=english&currency=1&item_nameid={marketid.group(1)}&two_factor=0")
     print("[INFO] Steam - BuyAPI: " + str(round((time.time()-s)*1000,0)) + " ms.")
-    assert r.status_code == 200 or r.status_code == 429
-    if r.status_code == 429:
-            lowestsell = "Ratelimited"
-            LSactualreturn = "Ratelimited"
-    else:
+    if r.status_code == 200:
         highestbuyr = re.search(r'to buy at.*?\$(\S{1,6}\.\d{2})', r.text)
         if highestbuyr is None:
             BOactualreturn = None
@@ -98,6 +101,10 @@ def GetPrices(item_attributes):
 
             #TODO: Review this callculation for actual return
             BOactualreturn = "$" + str("{:.2f}".format(round((float(BOactualreturn) / 1.15) + .005, 2)))
+    else:
+        print(f"Backend buyorders returned {r.status_code}, failed to scrape bid prices.")
+        highestbuy = "Failed"
+        BOactualreturn = "Failed"
 
     prices = {"Ask": lowestsell, "Bid": highestbuy, "Ask (net)": LSactualreturn, "Bid (net)": BOactualreturn}
     return prices
